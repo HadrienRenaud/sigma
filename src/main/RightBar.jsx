6c307754c0a1686d89312d24a4c02d904330d16f
Quentin Louis <quentinlouis@MacBook-Pro-de-Quentin.local>
2018-02-27 09:24:25 +0100

    Modification right ba

--- src/main/RightBar.jsx
+++ src/main/RightBar.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
+import _ from 'lodash';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import { Accordion, Button, Segment, Icon, Component } from 'semantic-ui-react';
 
-const RightBar = () => ( //TODO change into semantic-ui-react
-    <div>
-        <div className="compact menu">
-            <div className="ui simple dropdown item">
-                [Profile picture]
-                <div className="menu">
-                    <div className="item">Profil</div>
-                    <div className="item">Paramètres</div>
-                    <div className="item">Déconnexion</div>
-                </div>
-            </div>
-        </div>
+const RightBar = () => (
+    <div class="ui container">
+        <Segment.Group>
+        <Segment color="teal" inverted>
+            <p>Prénom Nom</p>
+        </Segment>
+        <Button.Group basic fluid vertical>
+            <Link to="/group/1"><Button>Paramètres</Button></Link>
+            <Link to="/group/1"><Button>Déconnexion</Button></Link>
+        </Button.Group>
+    </Segment.Group>
     </div>
 );
 
