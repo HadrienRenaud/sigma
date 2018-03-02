e4daab62786f086ddd1bd2b187cd50e935a85af9
Guillaume WANG <guillaume.wang@polytechnique.edu>
2018-03-02 12:15:26 +0100

    fix for QL

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ const app = express();
 
 // serving static files
 app.use(express.static('./build', {fallthrough: true}));
-app.use(express.static('./assets'));
+app.use(express.static('./src/assets'));
 
+// favicon
+app.use(favicon(path.resolve('./', 'src', 'assets', 'favicon.ico')));
 app.use((req,res,next) => { res.sendFile('./build/index.html', {root: './build/'}); });
 
 const port = process.env.NODE_PORT || 8888;
 
-// favicon
-app.use(favicon(path.resolve('./', 'src', 'assets', 'favicon.ico')));
 
 app.listen(port, () => {
     console.log( 'Express server listening on port %d in %s mode', port, app.settings.env );
---
+++ src/main/login/Login.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Route, Switch, Link, withRouter } from 'react-router-dom';
+import Error404 from '../../Errors.jsx';
+import { Button, Sticky, Icon, Popup } from 'semantic-ui-react';
+
+import LoginForm from './LoginForm.jsx';
+
+class LoginUnrouted extends React.Component {
+    state = {}
+
+    static propTypes = {
+        match: PropTypes.object.isRequired,
+        location: PropTypes.object.isRequired,
+        history: PropTypes.object.isRequired
+    }
+
+    setContextRef = contextRef => this.setState({ contextRef })
+
+    render() {
+        const { contextRef } = this.state;
+        const { match, location, history } = this.props;
+
+        return (
+            <div ref={this.setContextRef}>
+                coucou ici Login.jsx
+            </div>
+        );
+    }
+}
+
+const Login = withRouter(LoginUnrouted);
+
+const Group = ({ match }) => (
+    <div>
+        <Switch>
+            <Route path={match.url + "/list"} component={ListGroups} />
+            <Route path={match.url + "/:id"} component={GroupFound} />
+            <Route component={Error404} />
+        </Switch>
+    </div>
+);
+
+
+export default Login;
