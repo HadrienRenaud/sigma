9b596d48123a727d3b835f14f09e7812d93c58f6
Guillaume WANG <guillaume.wang@polytechnique.edu>
2018-03-02 11:33:02 +0100

    fixed augmentation for as Link

--- src/main/Header.jsx
+++ src/main/Header.jsx
@@ -4,56 +4,34 @@
 
 import React from 'react';
 import { Menu, Button, Image, Container } from 'semantic-ui-react';
-import { withRouter, Link, NavLink } from 'react-router';
+import { withRouter, Link, NavLink } from 'react-router-dom';
 import logo_sigma from '../assets/logo_sigma.png';
-
-import LoginForm from './LoginForm.jsx';
 
 class HeaderUnrouted extends React.Component {
 
-    
-    
-    state = { 
-        activeItem: 'trombi'
-    }
-
-    /**
-     * @function Change l'élément actif.
-     */
-
-    handleClick = (e, { name }) => this.setState({ activeItem: name })
-
-    
+    //state = { }
 
     render() {
-        //const activeItem = this.state.activeItem;
-
         return (
 
-            <Menu fixed='top' inverted>
-                
-                <Menu.Item as={Link} exact to='/' content="Status" />
-                <Menu.Item as={Link} to="/queue" content="Hold Queue" />
-                <Menu.Item as={NavLink} to="/messages" content="Messages" />
-                <Menu.Item as={NavLink} to="/stats" content="Stats" />
+            <Menu>
 
-                {/*
-                <Menu.Item as={Link} to='/home' content='Home'>
+                <Menu.Item as={NavLink} to='/home' content='Home'>
                     <Button> <Image alt='Logo' src={logo_sigma} size='mini' /> </Button>
                 </Menu.Item>
                 <Menu.Item>
-                    <Button as={Link} to='/calendar' content='Calendar'>Calendrier</Button>
+                    <Button as={NavLink} to='/calendar' content='Calendar'>Calendrier</Button>
                 </Menu.Item>
-                <Menu.Item as={NavLink} to='/trombi' name='trombi' active={activeItem === 'trombi'}>
+                <Menu.Item as={NavLink} to='/trombi' name='trombi'>
                     <Button>Trombinoscope</Button>
                 </Menu.Item>
-                <Menu.Item as={NavLink} to='/services' name='services' active={activeItem === name}>
+                <Menu.Item as={NavLink} to='/services' name='services'>
                     <Button>Services BR</Button>
                 </Menu.Item>
-                <Menu.Item as={NavLink} to='/createAccount' position='right' name='createAccount' active={activeItem === name}>
+                <Menu.Item as={NavLink} to='/createAccount' position='right' name='createAccount'>
                     <Button color="blue">Créer son compte</Button>
                 </Menu.Item>
-                */}
+
             </Menu>
 
         );
