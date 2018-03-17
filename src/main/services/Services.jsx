2a5adb911722ba4beb092d312c17facd08b8582d
ManifoldFR <wilson.jallet@gmail.com>
2018-03-17 11:53:18 +0100

    Exemple texte dans container

--- src/main/services/Services.jsx
+++ src/main/services/Services.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import { Container, Header, Image } from 'semantic-ui-react';
+import logo_gitlab from '../../assets/logo_gitlab_br.png';
 
-import { Header } from 'semantic-ui-react'; 
 class Services extends React.Component {
     constructor(props) {
         super(props);
@@ -9,23 +10,25 @@ class Services extends React.Component {
     render() {
         return (
             <div>
-                <h1>
-                    Services BR
-                </h1>
-                <p>
-                    Le BR propose tout un ensemble de services aux étudiants de l'École polytechnique : hébergement de serveurs, sites pour les associations, machines de calcul, dépannage...
-                    
-                    Il tient une permanence tous les mardis et jeudis de 19h15 à 20h15.
-                    
-                    Venez avec vos problèmes, le BR veille :)
-                </p>
-
-                <h2>
-                    <a href="https://gitlab.binets.fr">GitLab</a>
-                </h2>
-                <p>
-                    Le BR propose un GitLab, un service d'hébergement et de collaboration pour du code, en s'appuyant sur le système de contrôle de version Git : programmes en Python ou Java, applications JavaScript, rapports en LaTeX, tout ce que vous voulez !
-                </p>
+                <Container textAlign='justified'>
+                    <Header as='h1'>
+                        Services BR
+                    </Header>
+                    <p>
+                        Le BR propose tout un ensemble de services aux étudiants de l'École polytechnique : hébergement de serveurs, sites pour les associations, machines de calcul, dépannage...
+                        
+                        Il tient une permanence tous les mardis et jeudis de 19h15 à 20h15.
+                        
+                        Venez avec vos problèmes, le BR veille :)
+                    </p>
+                    <Header as='h2'>
+                        <a href="http://gitlab.binets.fr">GitLab</a>
+                    </Header>
+                    <Image src={logo_gitlab} size='large' centered/>
+                    <p>
+                        GitLab est un service d'hébergement et de gestion de fichiers particulièrement adapté à du code informatique : que ça soit pour un PSC, un projet de cours de maths ou d'info, un P3A, un modal, du travail pour X-Projets, un site internet pour un binet ou association, vous pouvez y mettre ce que vous voulez.
+                    </p>
+                </Container>
             </div>
         );
     }
