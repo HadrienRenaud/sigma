3db03426f338690115f638977c7d926863ef4380
Hadrien RENAUD <hadrien.renaud@polytechnique.edu>
2019-06-08 10:30:28 +0200

    Brouillon pour la page administrer.

--- src/main/groups/group_view/GroupAdministrer.jsx
+++ src/main/groups/group_view/GroupAdministrer.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
 
-import { Button, Segment, Icon, Divider, Card } from 'semantic-ui-react';
+import {Button, Segment, Icon, Divider, Card, Form, Header, Search} from 'semantic-ui-react';
 
 const GroupAdministrer = ({ match }) => (
     <div>
-        Group Administrer
-        TODO
+
+        <Segment basic>
+            <Header as="h3" content="Général"/>
+
+            <Form>
+                <Form.Group inline>
+                <Form.Input label="Nom" placeholder="The beautiful name you want to give your group" width={3}/>
+                <Button>Edit</Button>
+                </Form.Group>
+
+                <Divider hidden/>
+
+                <Form.TextArea label="Description" placeholder="The description of your group" />
+                <Button>Edit</Button>
+
+                <Divider hidden/>
+
+                <Header as="h4"> Picture</Header>
+            </Form>
+        </Segment>
+
+        <Segment basic>
+            <Header as="h3" content="Ajouter des membres"/>
+
+            <Search fluid/>
+        </Segment>
+
     </div>
 );
 
