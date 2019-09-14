1c56b6f1277e12be46192f0943964ef74a07589a
Hadrien RENAUD <hadrien.renaud@polytechnique.edu>
2019-09-14 17:01:00 +0200

    :tada: First page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import client from "./services/apollo/client";
+import {ApolloProvider} from '@apollo/react-hooks';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Login from "./pages/login/Login";
 
 const App: React.FC = () => {
-  return (
-    <div>
-      Hello World!
-    </div>
-  );
-}
+    return (
+        <ApolloProvider client={client}>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/" component={Login}/>
+                </Switch>
+            </BrowserRouter>
+        </ApolloProvider>
+    );
+};
 
 export default App;
