4fc79f1cefe48769a435e0c46672b1042e1a7cd2
Hadrien RENAUD <hadrien.renaud@polytechnique.edu>
2019-09-17 18:15:16 +0200

    :sparkles: Add Member page in group page

--- src/pages/group/GroupPage.tsx
+++ src/pages/group/GroupPage.tsx
@@ -1,22 +1,26 @@
-import React, {useContext} from "react";
+import React, {useContext, useState} from "react";
 import {Container, Menu} from "semantic-ui-react";
 import Main from "./components/Main";
-import {NavLink, Route, Switch} from "react-router-dom";
 import UserContext from "../../components/UserContext/context";
 import FrontPage from "./components/FrontPage";
 import {gql} from "apollo-boost";
 import {groupBase} from "../../services/apollo/fragments/group";
-import {useQuery} from "@apollo/react-hooks";
+import {useLazyQuery, useQuery} from "@apollo/react-hooks";
 import {Group} from "../../constants/types";
 import GraphQLError from "../../components/Messages/Errors";
+import Members, {GET_MEMBERS} from "./components/Members";
 
 export interface GroupPageProps {
     match: {
         params: {
-            group?: string
+            group: string
         }
-        url: string
     }
+}
+
+enum Tabs {
+    FRONT_PAGE,
+    MEMBERS,
 }
 
 const GET_GROUP = gql`
@@ -28,31 +32,29 @@ const GET_GROUP = gql`
     ${groupBase}
 `;
 
-const SUB_ROUTES = {
-    FRONT_PAGE: "/",
-} as const;
-
 function GroupPage(props: GroupPageProps) {
     const match = props.match;
     const gid = match.params.group || "";
+    const [getMembers] = useLazyQuery<{}, { gid: string }>(GET_MEMBERS);
     const {data, error, loading} = useQuery<{ group: Group }, { gid: string }>(
         GET_GROUP,
         {
-            variables: {gid}
+            variables: {gid},
+            onCompleted: () => getMembers({variables: {gid}})
         }
     );
     const group = gid && data && data.group;
     const {user} = useContext(UserContext);
 
     let isAdmin = false;
-    let isMember = false;
     let isSpeaker = false;
 
     if (gid && user) {
-        isMember = user.memberOf.map(gr => gr.gid).indexOf(gid) !== -1;
-        isAdmin = user.adminOf.map(gr => gr.gid).indexOf(gid) !== -1;
         isSpeaker = user.speakerOf.map(gr => gr.gid).indexOf(gid) !== -1;
+        isAdmin = user.adminOf.map(gr => gr.gid).indexOf(gid) !== -1;
     }
+
+    const [selectedTab, setSelectedTab] = useState<Tabs>(Tabs.FRONT_PAGE);
 
     return (
         <Container>
@@ -63,39 +65,21 @@ function GroupPage(props: GroupPageProps) {
             )}
             <Menu pointing secondary color="purple">
                 <Menu.Item
-                    as={NavLink} exact to={match.url}
+                    active={selectedTab === Tabs.FRONT_PAGE}
+                    onClick={() => setSelectedTab(Tabs.FRONT_PAGE)}
                     content="Page d'accueil"/>
                 <Menu.Item
-                    as={NavLink} to={match.url + "/annonces"}
-                    content="Annonces"/>
-                <Menu.Item
-                    as={NavLink} to={match.url + "/qanda"}
-                    content="Questions-Réponses"/>
-                <Menu.Item
-                    as={NavLink} to={match.url + "/events"}
-                    content="Événements"/>
-                <Menu.Item
-                    as={NavLink} to={match.url + "/members"}
+                    active={selectedTab === Tabs.MEMBERS}
+                    onClick={() => setSelectedTab(Tabs.MEMBERS)}
                     content="Membres"/>
-                {isMember && (
-                    <Menu.Item
-                        as={NavLink} to={match.url + "/interne"}
-                        position='right'
-                        content="Page interne"/>
-                )}
-                {isAdmin && (
-                    <Menu.Item
-                        as={NavLink} to={match.url + "/admin"}
-                        content="Administrer"/>
-                )}
             </Menu>
-            <Switch>
-                {group && (
-                    <Route path={SUB_ROUTES.FRONT_PAGE} render={() => (
-                        <FrontPage frontPage={group.frontPage || ""} isSpeaker={isSpeaker} gid={group.gid}/>
-                    )}/>
-                )}
-            </Switch>
+
+            {selectedTab === Tabs.MEMBERS && (
+                <Members gid={gid} isAdmin={isAdmin}/>
+            )}
+            {group && selectedTab === Tabs.FRONT_PAGE && (
+                <FrontPage frontPage={group.frontPage || ""} isSpeaker={isSpeaker} gid={group.gid}/>
+            )}
         </Container>
     );
 }
