3865eba246250ce704a02dea91f574a688cc05bd
Hadrien RENAUD <hadrien.renaud@polytechnique.edu>
2019-09-17 18:10:49 +0200

    :card_file_box: Add groupExtended fragment

--- src/services/apollo/fragments/group.ts
+++ src/services/apollo/fragments/group.ts
@@ -1,4 +1,6 @@
 import {gql} from "apollo-boost";
+import {userBase} from "./user";
+import {Group, User} from "../../../constants/types";
 
 export const groupBase = gql`
     fragment groupBase on Group {
@@ -19,3 +21,57 @@ export interface GroupBase {
     name: string
     website: string
 }
+
+export const groupExtended = gql`
+    fragment groupExtended on Group {
+        ...groupBase
+        __typename
+        ... on SimpleGroup {
+            admins {
+                ...userBase
+                photo
+            }
+            members {
+                ...userBase
+                photo
+            }
+            speakers {
+                ...userBase
+                photo
+            }
+            likers {
+                ...userBase
+                photo
+            }
+        }
+        ...on MetaGroup {
+            admins {
+                ...userBase
+                photo
+            }
+            members {
+                ...groupBase
+            }
+        }
+    }
+    ${groupBase}
+    ${userBase}
+`;
+
+
+export interface SimpleGroup extends Group {
+    __typename: "SimpleGroup"
+    admins: Array<User>
+    members: Array<User>
+    speakers: Array<User>
+    likers: Array<User>
+}
+
+export interface MetaGroup extends Group {
+    __typename: "MetaGroup"
+    admins: Array<User>
+    members: Array<Group>
+}
+
+export type GroupExtended = MetaGroup | SimpleGroup
+
